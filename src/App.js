5a573e888b35221923b8ceed5e5e128237f248ec
Sean Knox <knoxville@gmail.com>
2021-10-24 14:30:18 -0700

    add SearchParam component using Hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
-    [
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Sudo",
-        animal: "Dog",
-        breed: "Wheaten Terrier",
-      }),
-    ],
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
